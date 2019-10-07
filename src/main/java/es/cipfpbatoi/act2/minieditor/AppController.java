12b1095555031bb9cc26bbda34d9af5ef0cbb296
JavierJuanUceda <45400348+JavierJuanUceda@users.noreply.github.com>
2019-10-07 23:25:40 +0200

    Dia 07 de octubre de 2019 a las 23:25

--- src/main/java/es/cipfpbatoi/act2/minieditor/AppController.java
+++ src/main/java/es/cipfpbatoi/act2/minieditor/AppController.java
@@ -1,23 +1,24 @@
 package es.cipfpbatoi.act2.minieditor;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -31,6 +32,7 @@ import javafx.stage.Stage;
  */
 public class AppController implements Initializable {
 
+	static final int klimite = 5;
 	@FXML
 	private TextArea taEditor;
 	@FXML
@@ -46,213 +48,214 @@ public class AppController implements Initializable {
 	@FXML
 	private ChoiceBox archivos_recientes;
 	private boolean guardar;
-	private Stage escenario;
-	private File f;
+	private static Stage escenario;
+	private static File f;
 
+	
 	/**
-	 * Initializes the controller class.
+	 *@param URL url 
+	 *@param ResourceBundle rb
 	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 
 	}
 
+	/**
+	 * Metodo que crea un fichero nuevo.
+	 */
 	public void handleNuevo() {
-		boolean texto_vacio = false;
-		texto_vacio = taEditor.getText().isEmpty();
+		Vacio();
 		taEditor.setDisable(false);
-		if (texto_vacio == false) {
-
-			try {
-				handleGuardar();
-				taEditor.setText("");
-			} catch (Exception e) {
-				taEditor.setText("");
-				lblInfo.setText("");
-			}
-
-		}
+		taEditor.setText("");
 		guardar = true;
 	}
 
+	/**
+	 * Metodo que abre un documento con la clase Filechooser para elegir los archivos.
+	 */
 	public void handleAbrir() {
 
-		boolean texto_vacio = false;
-		texto_vacio = taEditor.getText().isEmpty();
-		taEditor.setDisable(false);
-		String texto_a_abrir = "";
-		String informacion = "";
-		int contador = 0;
-		if (texto_vacio == false) {
-
-			try {
-				handleGuardar();
-				taEditor.setText("");
-			} catch (Exception e) {
-				FileChooser eleccion = creaAbrirFileChooser("Abrir un archivo");
-			}
-
-			guardar = false;
-
-		}
+		Vacio();
+		guardar = false;
 
 		FileChooser eleccion = creaAbrirFileChooser("Abrir un archivo");
 		Stage escenario = new Stage();
 		f = eleccion.showOpenDialog(escenario);
-		Path archivo_a_guardar = Paths.get(f.getAbsolutePath());
-		List<String> todoElTexto = null;
-		try {
-			todoElTexto = Files.readAllLines(archivo_a_guardar);
-		} catch (IOException e) {
-			
-		}
-		for (String l : todoElTexto) {
-			texto_a_abrir = texto_a_abrir + l + "\n";
-			contador = contador + 1;
-		}
-		taEditor.setText(texto_a_abrir);
-		informacion = "Archivo: " + f.getName() + " tiene " + contador + " lineas, Tiene "
-				+ (texto_a_abrir.length() - 1) + " caracteres.";
-		lblInfo.setText(informacion);
+		Abrir();
+		archivosRecientes();
 
 	}
 
+	/**
+	 * Guarda un documento llamando a la funcion guardar.
+	 */
 	public void handleGuardar() {
-		int contador = 0;
-		if (guardar == false) {
-			Path archivo_a_guardar = Paths.get(f.getAbsolutePath());
-			String[] Texto = taEditor.getText().split("\n");
-			List<String> texto_a_guardar = new ArrayList();
-			for (contador = 0; contador < Texto.length; contador++) {
-				texto_a_guardar.add(Texto[contador]);
-			}
 
-			try {
-				Files.write(archivo_a_guardar, texto_a_guardar, StandardCharsets.UTF_8);
-			} catch (IOException e) {
-				
-			}
-		} else {
-			FileChooser eleccion = creaAbrirFileChooser("Guardar un archivo");
-			Stage escenario = new Stage();
-			f = eleccion.showSaveDialog(escenario);
-			contador = 0;
-			Path archivo_a_guardar = Paths.get(f.getAbsolutePath());
-			String[] Texto = taEditor.getText().split("\n");
-			List<String> texto_a_guardar = new ArrayList();
-			for (contador = 0; contador < Texto.length; contador++) {
-				texto_a_guardar.add(Texto[contador]);
-			}
-
-			try {
-				Files.write(archivo_a_guardar, texto_a_guardar, StandardCharsets.UTF_8);
-			} catch (IOException e) {
-				
-			}
-
-			archivosRecientes();
-			guardar=false;
-		}
-
-	}
-
-	public void handleCerrar() {
-
-	}
-
-	public void reciente() {
-
-		boolean texto_vacio = false;
-		texto_vacio = taEditor.getText().isEmpty();
-		taEditor.setDisable(false);
-		String texto_a_abrir = "";
-		String informacion = "";
-		int contador = 0;
-		String ruta = "";
-		Path archivo_a_guardar = null;
-		ruta = archivos_recientes.getItems().get(archivos_recientes.getSelectionModel().getSelectedIndex()).toString();
-		if (texto_vacio == false) {
-
-			try {
-				handleGuardar();
-				taEditor.setText("");
-			} catch (Exception e) {
-				f = new File(ruta);
-			}
-
-		}
-		f = new File(ruta);
-		archivo_a_guardar = Paths.get(f.getAbsolutePath());
-		List<String> todoElTexto = null;
-		try {
-			todoElTexto = Files.readAllLines(archivo_a_guardar);
-		} catch (IOException e) {
-			
-		}
-		for (String l : todoElTexto) {
-			texto_a_abrir = texto_a_abrir + l + "\n";
-			contador = contador + 1;
-		}
-		taEditor.setText(texto_a_abrir);
-		informacion = "Archivo: " + f.getName() + " tiene " + contador + " lineas, Tiene "
-				+ (texto_a_abrir.length() - 1) + " caracteres.";
-		lblInfo.setText(informacion);
-
-	}
-
-	public void archivosRecientes() {
-		Path archivo_a_guardar = Paths.get(".", "archivos_recientes.txt");
-		List<String> contenido = null;
-		List<String> todoElTexto = new ArrayList();
-		List<String> todoElTexto2 = new ArrayList();
-		List<String> todoElTexto_vacio = new ArrayList();
-		int contador = 0;
-		try {
-			contenido = Files.readAllLines(archivo_a_guardar);
-		} catch (IOException e1) {
+		Guardar();
+		archivosRecientes();
+		Cambiar_titulo_aplicacion();
 		
-		}
-		for (String l : contenido) {
-			todoElTexto.add(l);
-		}
-		todoElTexto.add(f.getAbsolutePath());
-		try {
-			Files.write(archivo_a_guardar, todoElTexto_vacio);
+		/*
+		 * Cambia guardar a false para que no pida guardar en un documento
+		 * nuevo cada vez.
+		 */
+		
+		guardar = false;
 
-			if (todoElTexto.size() < 5) {
-				Files.write(archivo_a_guardar, todoElTexto);
+	}
+
+	/**
+	 * Guarda en un documento nuevo.
+	 */
+	public void Guardarcomo() {
+		guardar = true;
+		Guardar();
+		guardar = false;
+
+	}
+
+	/**
+	 * Cierra el documento y desabilita el editor de textos.
+	 */
+	public void handleCerrar() {
+		Vacio();
+		handleNuevo();
+		taEditor.setDisable(true);
+	}
+
+	/**
+	 * Pone un documento reciente elegido en el desplegable.
+	 */
+	public void reciente() {
+		
+		try {
+			String ruta = "";
+			
+			/*
+			 * Pone la ruta del desplegable.
+			 */
+			
+			ruta = archivos_recientes.getItems().get(archivos_recientes.getSelectionModel().getSelectedIndex())
+					.toString();
+			Vacio();
+			f = new File(ruta);
+			Abrir();
+			
+		} catch (NullPointerException e) {
+			System.err.print("");
+		} catch (Exception e) {
+			System.err.println("Error general");
+		}
+
+	}
+
+	/**
+	 * Gestiona los archivos recientes.
+	 */
+	public void archivosRecientes() {
+
+		try {
+			Path archivo_a_guardar = Paths.get(".", "archivos_recientes.txt");
+			List<String> contenido = null;
+			List<String> todoElTexto = new ArrayList();
+			List<String> todoElTexto2 = new ArrayList();
+			List<String> todoElTexto_vacio = new ArrayList();
+			HashSet hs = null;
+			int contador = 0;
+			contenido = Leer_en_fichero(archivo_a_guardar);
+			
+			/*
+			 * hs elimina las rutas repetidas.
+			 */
+			
+			hs = new HashSet();
+			hs.addAll(contenido);
+			contenido.clear();
+			contenido.addAll(hs);
+			
+			/*
+			 * Anyade las lineas del fichero al arraylist.
+			 */
+			
+
+			for (String l : contenido) {
+				todoElTexto.add(l);
+			}
+			
+			/*
+			 * Anyade la ruta actual.
+			 */
+			
+			todoElTexto.add(f.getAbsolutePath());
+			Escribir_en_fichero(archivo_a_guardar, todoElTexto_vacio);
+			
+			
+			/*
+			 * Si esta debajo del limite lo escribe en el fichero.
+			 */
+
+			if (todoElTexto.size() < klimite) {
+				Escribir_en_fichero(archivo_a_guardar, todoElTexto);
 			} else {
-				for (contador = 5; contador > 0; contador--) {
+				
+				/*
+				 * Si no coge las ultimas rutas definidas en la constante y lo anyade
+				 * en el ArrayList.
+				 */
+
+				for (contador = klimite; contador > 0; contador--) {
 					todoElTexto2.add(todoElTexto.get(todoElTexto.size() - contador));
 				}
-				Files.write(archivo_a_guardar, todoElTexto2);
-			}
+				
+				/*
+				 * Lo escribe en el fichero.
+				 */
 
-		} catch (IOException e) {
-			
+				Escribir_en_fichero(archivo_a_guardar, todoElTexto2);
+			}
+		} catch (NullPointerException e) {
+			System.err.println("No se ha elegido un archivo");
+		} catch (Exception e) {
+			System.err.println("Error general");
 		}
 
 	}
 
+	/**
+	 * Muestra los archivos recientes en el desplegable.
+	 */
 	public void mostrar_archivosRecientes() {
+
 		archivos_recientes.getItems().clear();
 		List<String> todoElTexto = new ArrayList();
 		Path archivo_a_guardar = Paths.get(".", "archivos_recientes.txt");
-		try {
-			todoElTexto = Files.readAllLines(archivo_a_guardar);
-			for (String l : todoElTexto) {
 
-				archivos_recientes.getItems().add(l);
-
-			}
-
-		} catch (IOException e) {
+		todoElTexto = Leer_en_fichero(archivo_a_guardar);
 		
+		/*
+		 * Anyade las lineas en el ArrayList.
+		 */
+		
+		for (String l : todoElTexto) {
+
+			archivos_recientes.getItems().add(l);
+
 		}
+		
+		/*
+		 * Cambia guardable a false para que no pregunte si se guarda en otro
+		 * documento.
+		 */
+		
 		guardar = false;
 	}
 
-	private FileChooser creaAbrirFileChooser(String Titulo) {
+	/**
+	 * @param Titulo El titulo que tendra el elemento FileChooser
+	 * @return fc Devuelve el FileChhoser
+	 */
+	public FileChooser creaAbrirFileChooser(String Titulo) {
 		FileChooser fc = new FileChooser();
 		fc.setTitle(Titulo);
 		fc.setInitialDirectory(new File(System.getProperty("user.home")));
@@ -261,8 +264,205 @@ public class AppController implements Initializable {
 		return fc;
 	}
 
-	void setEscenario(Stage escenario) {
+	/**
+	 * @param archivo_a_guardar La variable del archivo en la que se escribira
+	 * @param texto_a_guardar El ArrayList del texto que se guardara en el fichero
+	 */
+	public void Escribir_en_fichero(Path archivo_a_guardar, List<String> texto_a_guardar) {
+		
+		/*
+		 * Crea el fichero.
+		 */
+		
+		try (BufferedWriter bw = Files.newBufferedWriter(archivo_a_guardar)) {
+
+			/*
+			 * Escribe el fichero linea a linea.
+			 */
+			
+			for (String linea : texto_a_guardar) {
+				bw.write(linea);
+				bw.newLine();
+			}
+
+		} catch (IOException e) {
+			System.err.println("Error en el sistema de entrada / salida con el mensaje: " + e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Abre el documento.
+	 */
+	public void Abrir() {
+
+		try {
+			String texto_a_abrir = "";
+			Path archivo_a_guardar = Paths.get(".", "archivos_recientes.txt");
+			List<String> todoElTexto = new ArrayList();
+			int contador = 0;
+			
+			taEditor.setDisable(false);
+			archivo_a_guardar = Paths.get(f.getAbsolutePath());
+			todoElTexto = Leer_en_fichero(archivo_a_guardar);
+			
+			/*
+			 * Escribe todas las lineas en el fichero.
+			 */
+			
+			for (String l : todoElTexto) {
+				texto_a_abrir = texto_a_abrir + l + "\n";
+				contador = contador + 1;
+			}
+			
+			Cambiar_titulo_aplicacion();
+			taEditor.setText(texto_a_abrir);
+			Mostrar_informacion();
+			
+		} catch (NullPointerException e) {
+			System.err.print("");
+		} catch (Exception e) {
+			System.err.println("Error general");
+		}
+
+	}
+
+	/**
+	 * Guarda un fichero
+	 */
+	public void Guardar() {
+		int contador = 0;
+
+		try {
+			
+			/*
+			 * Si la variable guardar es true se preguntara
+			 * si se quiere guardar en otro fichero y si no se sobreescribira
+			 * en el fichero.
+			 */
+			
+			if (guardar == true) {
+				FileChooser eleccion = creaAbrirFileChooser("Guardar un archivo");
+				Stage escenario = new Stage();
+				f = eleccion.showSaveDialog(escenario);
+			}
+			contador = 0;
+			Path archivo_a_guardar = Paths.get(f.getAbsolutePath());
+			String[] Texto = taEditor.getText().split("\n");
+			List<String> texto_a_guardar = new ArrayList();
+			
+			/*
+			 * Se anyadira en el ArrayList las lineas del elemento TextArea.
+			 */
+			
+			for (contador = 0; contador < Texto.length; contador++) {
+				texto_a_guardar.add(Texto[contador]);
+			}
+			
+			/*
+			 * Escribe el el fichero y muestra la informacion.
+			 */
+			
+			Escribir_en_fichero(archivo_a_guardar, texto_a_guardar);
+			Mostrar_informacion();
+
+		} catch (Exception e) {
+			System.err.print("");
+		}
+
+	}
+
+	/**
+	 * El metodo que decide si el campo de texto esta vacio.
+	 */
+	public void Vacio() {
+		boolean texto_vacio = taEditor.getText().isEmpty();
+		
+		/*
+		 * Si esta vacio presenta una alerta preguntando si quieres guardarlo.
+		 */
+		
+		if (texto_vacio == false) {
+
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Pregunta");
+			alert.setHeaderText("¿Quieres guardar el archivo?");
+			
+			Optional<ButtonType> option = alert.showAndWait();
+
+			/*
+			 * Si se aprieta al boton de OK pregunta donde ser guardado
+			 * y si no hace nada.
+			 */
+			
+			if (option.get() == ButtonType.OK) {
+				Guardar();
+			} else {
+				taEditor.setText("");
+			}
+		}
+	}
+
+	/**
+	 * @param archivo_a_guardar El path donde se guardara el fichero.
+	 * @return texto_del_fichero El texto en forma de ArrayList que almacenara el fichero.
+	 */
+	public List<String> Leer_en_fichero(Path archivo_a_guardar) {
+		
+		List<String> texto_del_fichero = new ArrayList();
+		
+		/*
+		 * El archivo de lectura.
+		 */
+		
+		try (BufferedReader br = Files.newBufferedReader(archivo_a_guardar)) {
+			
+			String linea = null;
+			
+			/*
+			 * Anyade todas las lineas leidas al ArrayList.
+			 */
+			
+			while ((linea = br.readLine()) != null) {
+				texto_del_fichero.add(linea);
+			}
+		
+		} catch (IOException e) {
+
+			System.err.println("Error en el sistema de entrada / salida con el mensaje: " + e.getMessage());
+		}
+		return texto_del_fichero;
+
+	}
+
+	/**
+	 * @param escenario que tiene la aplicacion
+	 */
+	public void setEscenario(Stage escenario) {
 		this.escenario = escenario;
+	}
+
+	/**
+	 * Muestra las lineas y los caracteres del fichero.
+	 */
+	public void Mostrar_informacion() {
+		String informacion = "";
+		informacion = "Archivo: " + f.getName() + " tiene " + taEditor.getText().split("\n").length + " lineas, Tiene "
+				+ (taEditor.getText().length() - 1) + " caracteres.";
+		lblInfo.setText(informacion);
+	}
+
+	/**
+	 * Cambia el titulo de la aplicacion
+	 */
+	public static void Cambiar_titulo_aplicacion() {
+		try {
+			escenario.setTitle("MINI EDITOR DE TEXTOS" + " " + f.getName());
+
+		} catch (Exception e) {
+			System.err.print("");
+		}
+
 	}
 
 }
